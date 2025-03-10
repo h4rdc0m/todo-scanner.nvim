29a8497f3edd06bcc11f047297fb48cfabf4afec
Christiaan Benedictus <chris@combat.nl>
2025-03-10 11:32:33 +0100

    trying to fix precomp

--- lua/todo-scanner.lua
+++ lua/todo-scanner.lua
@@ -38,7 +38,7 @@ local function compile_patterns()
     local todo_tags = table.concat(config.todo_tags, "|")
     local patterns = config.comment_patterns[extension] or {}
     for _, pattern in ipairs(patterns) do
-      compiled_patterns[extension] = pattern:gsub("{TODO_TAGS}", todo_tags)
+      table.insert(compiled_patterns[extension], pattern:gsub("{TODO_TAGS}", todo_tags))
     end
   end
 end
